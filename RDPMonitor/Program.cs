eedc53072f1c27053a4dc661a782d1dd63b90ec3
Ricardo Polo <ricardo.polo@tulpep.com>
2013-01-30 17:58:34 -0500

    First commit working

---
+++ RDPMonitor/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace RDPMonitor
+{
+    static class Program
+    {
+
+        [STAThread]
+        static void Main()
+        {
+
+            //Start MSTSC
+            Process mstsc = new Process();
+            mstsc.StartInfo.FileName = "mstsc.exe";
+            mstsc.StartInfo.Arguments = @"\\CAVDCP05\Netlogon\VDI.rdp";
+            mstsc.Start();
+                         
+
+            while (true)
+            {
+                Thread.Sleep(2000);
+
+                //Check if process exists
+                Process[] pname = Process.GetProcessesByName("mstsc");
+                if (pname.Length == 0)
+                {
+
+                    Process logoff = new Process();
+                    logoff.StartInfo.FileName = "logoff.exe";
+                    //Do Not show Window
+                    logoff.StartInfo.RedirectStandardOutput = true;
+                    logoff.StartInfo.UseShellExecute = false;
+                    logoff.StartInfo.CreateNoWindow = true;
+                    //
+                    logoff.Start();
+
+                }
+                                
+
+            }
+
+        }
+    }
+}
